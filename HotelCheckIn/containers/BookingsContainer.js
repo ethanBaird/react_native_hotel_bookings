b481c7e5b17dacc62b16fa92eb442286f9130496
Ethan <ethanbaird94@gmail.com>
2022-09-25 12:31:40 +0100

    renders bookings container

---
+++ HotelCheckIn/containers/BookingsContainer.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native"
+
+const BookingsContainer = () => {
+    return(
+        <Text>This is the bookings container</Text>
+    )
+}
+
+export default BookingsContainer
